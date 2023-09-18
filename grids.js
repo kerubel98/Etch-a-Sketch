26eecc3935e2a34da09b37c074c5906d8c120e7b
kerubel98 <62537548+kerubel98@users.noreply.github.com>
2023-09-19 02:54:34 +0400

    Final layout

--- grids.js
+++ grids.js
@@ -13,13 +13,22 @@
         function hove(e) {
             this.classList.add('newr');
         };
-        function addside(e) {
-            let dd = document.querySelector('.side_pnal');
-            dd.classList.add('side_pnal2');
-        };
         let news = document.querySelectorAll('.new');
         news.forEach(element =>{element.addEventListener("mouseover", hove)});
 
-        let link = document.querySelector('a');
-        link.addEventListener('click', addside)
+        var btn = document.querySelector('.toggle');
+        var btnst = true;
+        btn.onclick = function() {
+            if(btnst == true) {
+                document.querySelector('.toggle span').classList.add('toggle');
+                document.getElementById('sidebar').classList.add('sidebarshow');
+                btnst = false;
+            }else if(btnst == false) {
+                document.querySelector('.toggle span').classList.remove('toggle');
+                document.getElementById('sidebar').classList.remove('sidebarshow');
+                btnst = true;
+            }
+        }
+
+        
     
